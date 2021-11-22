0fb4a571d3b0aa5859fd06f580b9e810d1c76d75
yuzu <s.yuzu441@gmail.com>
2021-11-23 01:42:36 +0900

    Add counter.

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+  import CounterForm from './Counter.svelte'
+
   let name: string = 'world'
 </script>
 
 <h1 class="red">Hello {name}!</h1>
+<CounterForm />
 
 <style>
   .red {
---
+++ src/Counter.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  $: count = 0
+
+  function increment(event: MouseEvent) {
+    event.preventDefault()
+    count++
+  }
+</script>
+
+<div>
+  <p>{count}</p>
+  <button on:click={increment}>+1</button>
+</div>
