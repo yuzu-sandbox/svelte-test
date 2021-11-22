f665dbc3a79ca15b36e2bf4e8802b76f174e0e40
yuzu <s.yuzu441@gmail.com>
2021-11-23 01:57:27 +0900

    Add on submit and data binding.

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import CounterForm from './Counter.svelte'
+  import SubmitHandleForm from './SubmitHandleForm.svelte'
 
   let name: string = 'world'
 </script>
 
 <h1 class="red">Hello {name}!</h1>
 <CounterForm />
+<SubmitHandleForm />
 
 <style>
   .red {
---
+++ src/SubmitHandleForm.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+  $: v = 0
+
+  async function f(): Promise<number> {
+    return v + 1
+  }
+
+async function submitHandle(evt: Event & {
+  currentTarget: EventTarget & HTMLFormElement;
+}) {
+  event?.preventDefault()
+  const result = await f()
+  console.log('result', result)
+  v = result
+}
+
+let inputValue = ''
+
+</script>
+
+<form on:submit={submitHandle}>
+<p>{v} - {inputValue}</p>
+
+<input type="text" bind:value={inputValue} />
+<button type="submit">update</button>
+</form>
